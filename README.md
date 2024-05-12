56718e9f97e3bea44eecedf9e6c069e03c08e0a2
Jacopo_ <136124143+JacksoneDT@users.noreply.github.com>
2024-05-12 15:30:28 +0200

    New Repo

---
+++ README.md
@@ -0,0 +1,2 @@
+This is my first Web_Site project, really simple and full of mistakes.
+I started my path a month ago and all the time i will try to improve it with some new feature or aknowlegment that i´ll learn.
---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("insta").addEventListener("click", function () {
+  window.open("https://www.instagram.com/jacopo.diterlizzi/", "_blank");
+});
+document.getElementById("face").addEventListener("click", function () {
+  window.open("https://www.facebook.com/jditerlizzi1/", "_blank");
+});
+document.getElementById("linkedin").addEventListener("click", function () {
+  window.open(
+    "https://www.linkedin.com/in/jacopo-antonio-di-terlizzi-12947321b/",
+    "_blank"
+  );
+});
+
+document.getElementById("about-me").addEventListener("click", function () {
+  window.open("about_me/about_me.html", "_blank");
+});
